b9640aead3de437f0ce65c844d0f8170514210d9
Ruben <49733228+blogruben@users.noreply.github.com>
2022-12-27 09:34:07 +0100

    Resaltamos la palabra de la busqueda

--- Funcion-buscar-archivos-que-contengan-un-texto.ps1
+++ Funcion-buscar-archivos-que-contengan-un-texto.ps1
@@ -1,10 +1,46 @@
-#Funcion Segundaria para buscar archivos evitando ciertas carpetas y nombres de archivo
+#Funcion Segundaria para realzar la palabra buscada en amarillo
+function Resaltar-Texto {
+   param(
+   [Parameter(Mandatory, ValueFromPipeline)]
+   [string]$texto,
+   [string]$resaltar)
+   $lenTextoResalta = $resaltar.Length 
+   
+   $primerIndice = 0
+   $ultimoIndice = $texto.Length  
+   $indicesInicioResaltado = ($texto | Select-String $resaltar -AllMatches).Matches.Index
+   $indicesFinalResaltado = $indicesInicioResaltado | ForEach-Object {$PSItem + $lenTextoResalta}
+
+   #indices entre el texto resaltado y el normal
+   $indicesTodo = @($ultimoIndice,$primerIndice) 
+   $indicesTodo += $indicesInicioResaltado
+   $indicesTodo += $indicesFinalResaltado
+   $indicesTodo = $indicesTodo | Sort-Object
+
+   #Write-Host "indicesTodo"
+   #Write-Host $indicesTodo 
+   for ( $index = 0; $index -lt ($indicesTodo.count - 1); $index++ )
+   {
+       $long = $indicesTodo[$index+1]-$indicesTodo[$index]
+	   $imprimir = $texto.Substring($indicesTodo[$index],$long)
+       #Write-Host $indicesTodo[$index] $long $texto.Substring($indicesTodo[$index],$long)
+	   if($index%2 -eq 0) {
+          Write-Host  $imprimir -NoNewline; 
+       } 
+	   else {
+          Write-Host -ForegroundColor yellow $imprimir -NoNewline;
+	   }
+   }
+   Write-Host  ''; 
+}
+
+#Funcion Segundaria para buscar archivos evitando nombres en la ruta o en el archivo predeterminado.
 function Buscar-Archivos {
     param([string[]]$evitarDirectorio,[string[]]$evitarArchivo)
   
 	$evitarDirs = @('*.gradle*','*\classes\*','*\.metadata\*','*\.git\*')
 	$evitarDirs += $evitarDirectorio
-    $evitarFiles = @('*.class','*.bin')
+    $evitarFiles = @('*.class','*.bin','*.ear','*.war','*.exe')
 	$evitarFiles += $evitarArchivo
 	   
 	$dir = Get-ChildItem -Recurse
@@ -22,7 +58,7 @@ function Buscar-Archivos {
 
 #Funcion Segundaria para formatear la salido de las lineas de los archivos a mostrar
 function Mostrar-Lineas {
-    param([Object[]]$lineas,[String[]]$textoBuscado)
+    param([Object[]]$lineas,[String]$textoBuscado)
     Foreach($linea in $lineas) {
 	     #Acortar la ruta del archivo, mostrar la ruta relativa de la posicion donde estamos, y no la ruta absoluta
 		  $miPathLongitud = (Get-Location  | Select-Object -ExpandProperty 'Path').Length
@@ -30,14 +66,17 @@ function Mostrar-Lineas {
      	 Add-Member -InputObject $linea  -NotePropertyName "ruta" -NotePropertyValue $rutarelativa -erroraction 'silentlycontinue'
 		 #En el texto que buscamos se usan las tabulaciones, pero al mostrarlo en la consola se descoloca todo, cambiamos las tabulaciones por espacios
 		 $lineasSinTabulaciones = $linea.Line.trim().Replace("`t", " ")
+		 
 		 Add-Member -InputObject $linea  -NotePropertyName "LineaSinEspacios" -NotePropertyValue $lineasSinTabulaciones
     }
 	 # Mostramos estas tres columnas y con wrap hacemos que el contenido no salte a otras columnas cuando no entra en el ancho.
 	 
-	 Write-output ""
-	 Write-output "                                              ******* $textoBuscado *******"
-	 Write-output $lineas | Select ruta,LineNumber,LineaSinEspacios | Format-Table -Wrap
-	
+	 Write-Host ""
+	 Write-Host "" 
+	 Write-Host ""
+	 Write-Host "                                              ******* $textoBuscado *******"  -ForegroundColor Cyan
+	 Write-output $lineas | Select ruta,LineNumber,LineaSinEspacios | Format-Table -Wrap | out-string | resaltar-texto -resalta $textoBuscado
+	 #resaltar-texto Write-host $mostrar -resalta "we" 
 }
 
 #Funcion Principal para buscar todos los archivos que contengan un texto
@@ -50,5 +89,4 @@ function Buscar-Texto {
    }
 }
 
-#Ejemplo que buscar textos en todos los archivos de esta carpeta recursivamente
-#Buscar-Texto -Texto "URL_NAS","java.net.URL"
+#Buscar-Texto -Texto "CONSULTA","import","javax"
